55274d852caeac612e44025ed20bfb7a4987ae67
eunflash00 <eunflash00@gsa.hs.kr>
2022-10-11 10:26:49 +0900

    그랩마켓 원격서버

---
+++ index.js
@@ -0,0 +1,28 @@
+//index.js
+var http = require("http"); // node 내장 모듈 불러옴
+var hostname = "127.0.0.1"; // localhost와 동일
+var port = 3000;
+
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "농구공",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다.");
+    }
+  }
+  res.end("Good Bye");
+});
+
+server.listen(port, hostname);
+
+console.log("Server running at http://" + hostname + ":" + port);
